8e79cac70846db0aea52be809a3633b385fd86bd
clvalada <clvalada@gmail.com>
2023-11-27 10:21:38 -0500

    updated app.js

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
+// app.js
+
 const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const dotenv = require('dotenv');
+const { sequelize } = require('./models'); // Assuming you have a 'models' folder with Sequelize models
+
+dotenv.config();
+
 const app = express();
-const postRoutes = require('./routes/postroutes');
+const PORT = process.env.PORT || 3000;
 
-// Use the postRoutes in your app
-app.use('/api', postRoutes);
+// Set up Handlebars as the view engine
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
-// Other setup code...
+// Set up session management
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
+  })
+);
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// Set up routes and controllers
+app.use('/api', require('./routes/postRoutes')); // Example route, adjust based on your project structure
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
