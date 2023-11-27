58ac9fafb4893f7e455231356184a5e7748b3d0b
clvalada <clvalada@gmail.com>
2023-11-27 13:53:20 -0500

    updating js

--- controllers/postcontroller.js
+++ controllers/postcontroller.js
@@ -1,9 +1,14 @@
-// PostController.js
-exports.getHomePage = (req, res) => {
-    // Logic to fetch posts or other data
-    const posts = /* ... */;
-  
+const { Post } = require('../models');
+
+exports.getHomePage = async (req, res) => {
+  try {
+    // Fetch posts from the database
+    const posts = await Post.findAll();
+
     // Render the 'home' view with the posts data
     res.render('home', { posts });
-  };
-  
+  } catch (error) {
+    console.error('Error fetching posts:', error);
+    res.status(500).send('Internal Server Error');
+  }
+};
