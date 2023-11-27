1aaeaf028950750795ff863982f5fe8631458102
clvalada <clvalada@gmail.com>
2023-11-27 10:05:05 -0500

    file structure

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const postRoutes = require('./routes/postRoutes');
+
+// Use the postRoutes in your app
+app.use('/api', postRoutes);
+
+// Other setup code...
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
