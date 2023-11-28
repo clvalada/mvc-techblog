729eaa739623775a0f5a78c35c240d61c6c9d2df
clvalada <clvalada@gmail.com>
2023-11-28 12:26:29 -0500

    editing post routes

--- controllers/postcontroller.js
+++ controllers/postcontroller.js
@@ -1,14 +1,47 @@
-const { Post } = require('../models');
+// postcontroller.js
+
+// Import any necessary dependencies or models
+const Post = require('../models/post')
+
+// Controller functions
+exports.getAllPosts = async (req, res) => {
+  try {
+    // Logic to fetch all posts
+    const posts = await Post.findAll();
+    res.json(posts);
+  } catch (error) {
+    console.error('Error fetching posts:', error);
+    res.status(500).send('Internal Server Error');
+  }
+};
+
+exports.getPostById = async (req, res) => {
+  // Logic to fetch a post by ID
+  // ...
+};
 
 exports.getHomePage = async (req, res) => {
   try {
-    // Fetch posts from the database
+    // Logic to fetch posts or other data for the home page
     const posts = await Post.findAll();
-
-    // Render the 'home' view with the posts data
     res.render('home', { posts });
   } catch (error) {
     console.error('Error fetching posts:', error);
     res.status(500).send('Internal Server Error');
   }
+};
+
+exports.createPost = async (req, res) => {
+  // Logic to create a new post
+  // ...
+};
+
+exports.updatePost = async (req, res) => {
+  // Logic to update a post by ID
+  // ...
+};
+
+exports.deletePost = async (req, res) => {
+  // Logic to delete a post by ID
+  // ...
 };
