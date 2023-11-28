90417e38b6b655158280292da5d7b84683a3d96d
clvalada <clvalada@gmail.com>
2023-11-28 11:30:30 -0500

    editing js

--- database.js
+++ database.js
@@ -1,4 +1,3 @@
-// config.js
 require('dotenv').config();
 
 module.exports = {
@@ -7,7 +6,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'mysql', // Change this to your database dialect (mysql, postgres, sqlite, etc.)
-  },
-  // You might need similar configurations for 'test' and 'production' environments
+    dialect: 'mysql', 
+  }
+  
 };
